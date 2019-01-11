6400169c56c4760681148b67178ffb26372aab64
huhsame <seungyeon.huh@wrl.onl>
2019-01-11 23:41:28 +0900

    gun server setting

--- public/javascripts/gun/init.js
+++ public/javascripts/gun/init.js
@@ -1,3 +1,12 @@
 const gun = Gun('http://192.168.1.77:3000/gun');
 let test = 'scene0110'
 let sceneG = gun.get('root').get(test);
+let namesG = gun.get('root').get('names');
+
+// namesG.set('a');
+// namesG.set('b');
+// namesG.set('c');
+
+namesG.open(( data )=>{
+    console.log(data);
+});
